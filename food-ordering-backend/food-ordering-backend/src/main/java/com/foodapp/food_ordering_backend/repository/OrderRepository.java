27934d89dbf0404203acc7b708133a9f4d9a8aba
Deepaligupta0150 <155942079+Deepaligupta0150@users.noreply.github.com>
2025-10-15 16:45:18 +0530

    Project

--- food-ordering-backend/food-ordering-backend/src/main/java/com/foodapp/food_ordering_backend/repository/OrderRepository.java
+++
@@ -1,6 +0,0 @@
-package com.foodapp.food_ordering_backend.repository;
-
-import com.foodapp.food_ordering_backend.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {}
